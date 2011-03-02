e1c8a6d71808b83be63223dec666ff9b82ba09d1
Thomas S Hatch <thatch45@gmail.com>
2011-03-02 10:46:12 -0700

    laid out the pin module

--- clay/pin.py
+++ clay/pin.py
@@ -2,3 +2,44 @@
 The simple interface to pin images to hypervisors, this interface is required
 by many components.
 '''
+import os
+
+class Pin(object):
+    '''
+    Interface to the clay pinning system
+    '''
+    def __init__(self, opts):
+        '''
+        Create a pin object, pass in the opts dict
+        '''
+        self.opts = opts
+        self.instance = self.__gen_instance()
+
+    def __gen_instance(self):
+        '''
+        Figures out what the instance directory should be and returns it.
+        '''
+        pri = os.path.join(self.opts['instances'], self.opts['name'])
+        sec = os.path.join(self.opts['instances'], self.opts['fqdn'])
+        if os.path.isdir(pri):
+            return pri
+        if os.path.isdir(sec):
+            return sec
+        return pri
+
+    def _check_pins(self):
+        '''
+        Check the instances dir to see if this image has been previously
+        pinned.
+        '''
+        pinfile = os.path.join(self.instance, 'pin')
+        if os.path.isfile(pinfile):
+            return open(pinfile, 'r').read().strip()
+        return ''
+
+    def _set_pin_data(self, pin_data):
+        '''
+        Create the pin file.
+        '''
+        pinfile = os.path.join(self.instance, 'pin')
+        open(pinfile, 'w+').write(pin_data)
